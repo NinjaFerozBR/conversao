aacd717a852c7968be8dee54fe69c931169d03ad
NinjaFerozBR <165736778+NinjaFerozBR@users.noreply.github.com>
2024-05-27 21:28:30 -0300

    Add files via upload
    atualizado

--- teste2.py
+++ teste2.py
@@ -1,22 +1,25 @@
-#Variantes Dolar e Reak
+#Crie um sistema para converter real em dollar 
+
+#Variantes Dolar e Real
 dolar = 1
 
 real = 1
 
 #Opção para digitar
 
-opcao = int(input("Digite a opção (1 para converter real/dolar ou 2 para dolar/real): "))
+opcao = int(input("Digite a opção \n 1- para converter real/dolar \n 2- para dolar/real) \n : "))
 
 
-if opcao == 1: # Real*Dolar
+if opcao ==1: # Real*Dolar
     moeda = float(input("Digite o valor que deseja converter "))
     resultado = dolar * 5.19
     print(f"o valor em reais Convertidos para Dolar é {resultado}")
-
-elif dolar == 2: #Dolar/Real
+elif opcao ==2: # Dolar/Real
     moeda = float(input("Digite o valor que deseja converter "))
     resultado = dolar / 5.19
-    print(f"o valor em reais Convertidos para Dolar é {resultado}")
+    print(f"o valor em dolar Convertidos para reais é {resultado}")
+else:
+    print("Opcao Invalida")
 
     
 
